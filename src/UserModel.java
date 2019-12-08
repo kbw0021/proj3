0fe1ce0b887c3112b54755acb6ff11098c6bab6c
kbw0021 <kbw0021@auburn.edu>
2019-12-07 22:04:36 -0600

    Update UserModel.java

--- src/UserModel.java
+++ src/UserModel.java
@@ -1,7 +1,7 @@
 public class UserModel {
     public static final int CUSTOMER = 0;
     public static final int CASHIER = 1;
-    public static final int MANAGER = 2;
+    public static final int MANAGER = 2; 
     public static final int ADMIN = 3;
 
 
