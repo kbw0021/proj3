7e0a84a6578306e0498d80fdaf3b0fb7fa959f80
kbw0021 <kbw0021@auburn.edu>
2019-12-07 22:02:10 -0600

    Update ManageProductUI.java

--- src/ManageProductUI.java
+++ src/ManageProductUI.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.PrintWriter;
+import java.io.PrintWriter; 
 import java.net.Socket;
 import java.util.Scanner;
 
