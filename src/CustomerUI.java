8c485cadb7d1d0e411aab859db7efd915246ed81
kbw0021 <kbw0021@auburn.edu>
2019-12-07 22:00:11 -0600

    Update CustomerUI.java

--- src/CustomerUI.java
+++ src/CustomerUI.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+ 
 public class CustomerUI {
 
     public UserModel user = null;
