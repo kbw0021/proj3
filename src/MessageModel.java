6d0b2b599d465dcb67fe6b93c6fae081c462824d
kbw0021 <kbw0021@auburn.edu>
2019-12-07 22:02:33 -0600

    Update MessageModel.java

--- src/MessageModel.java
+++ src/MessageModel.java
@@ -4,7 +4,7 @@ public class MessageModel {
     public static final int PUT_PRODUCT = 101;
 
     public static final int GET_CUSTOMER = 200;
-    public static final int PUT_CUSTOMER = 201;
+    public static final int PUT_CUSTOMER = 201; 
 
     public static final int GET_PURCHASE = 300;
     public static final int PUT_PURCHASE = 301;
