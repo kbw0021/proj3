06ee5f224ce51b90ede3f27f5d770e27f95d4471
kbw0021 <kbw0021@auburn.edu>
2019-12-07 21:59:31 -0600

    Update AddPurchaseUI.java

--- src/AddPurchaseUI.java
+++ src/AddPurchaseUI.java
@@ -3,7 +3,7 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.ActionListener; 
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.util.Calendar;
