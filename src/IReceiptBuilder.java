4eac58665832c93e1974b09518a3a71a5d28330d
kbw0021 <kbw0021@auburn.edu>
2019-12-07 22:01:00 -0600

    Update IReceiptBuilder.java

--- src/IReceiptBuilder.java
+++ src/IReceiptBuilder.java
@@ -1,6 +1,6 @@
 public interface IReceiptBuilder {
     public void appendHeader(String header);
-    public void appendCustomer(CustomerModel customer);
+    public void appendCustomer(CustomerModel customer); 
     public void appendProduct(ProductModel product);
     public void appendPurchase(PurchaseModel purchase);
     public void appendFooter(String footer);
